c9be25148140738d79a7f8a06b6ca6c9e3f92eac
Marius Darila <marius.darila@gmail.com>
2016-10-18 21:02:04 +0300

    fix(objectLink): prevent checking for multihash on files

--- src/IpfsApiHelper.ts
+++ src/IpfsApiHelper.ts
@@ -246,7 +246,7 @@ export class IpfsApiHelper {
     public constructObjLink(data: any, isProtobuf = false) {
         const constructed = {};
         constructed[IpfsApiHelper.ENC_SYMBOL] = (isProtobuf) ? IpfsApiHelper.ENC_PROTOBUF : IpfsApiHelper.ENC_BASE58;
-        if (multihash(data)) {
+        if (!isProtobuf && multihash(data)) {
             constructed[IpfsApiHelper.LINK_SYMBOL] = data;
             return Promise.resolve(constructed);
         }

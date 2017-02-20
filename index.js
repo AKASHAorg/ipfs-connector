fa965e05227f8cae9af01e2640bca7ac10278d74
Marius Darila <marius.darila@gmail.com>
2017-02-20 13:06:30 +0200

    chore(): update go-ipfs version 0.4.5

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 "use strict";
-var IpfsConnector_1 = require('./src/IpfsConnector');
+var IpfsConnector_1 = require("./src/IpfsConnector");
 exports.IpfsConnector = IpfsConnector_1.IpfsConnector;
-var IpfsApiHelper_1 = require('./src/IpfsApiHelper');
+var IpfsApiHelper_1 = require("./src/IpfsApiHelper");
 exports.IpfsApiHelper = IpfsApiHelper_1.IpfsApiHelper;
-var IpfsBin_1 = require('./src/IpfsBin');
+var IpfsBin_1 = require("./src/IpfsBin");
 exports.IpfsBin = IpfsBin_1.IpfsBin;
-var constants_1 = require('./src/constants');
+var constants_1 = require("./src/constants");
 exports.ipfsEvents = constants_1.events;

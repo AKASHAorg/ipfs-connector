fde2582c29e4e84531b127c318dd12e74cb60ce0
Marius Darila <marius.darila@gmail.com>
2017-05-04 19:19:19 +0300

    chore(): travis

---
+++ typings/main.d.ts
@@ -0,0 +1,18 @@
+declare module 'bin-wrapper';
+declare module 'ipfs';
+declare module 'ipfs-api';
+declare module 'ipld';
+declare module 'is-ipfs';
+declare module 'ipld-dag-pb';
+
+interface Connector {
+    api: any;
+    setLogger(logger: {}): void;
+    setConfig(option: string, value: string): void;
+    setIpfsFolder(target: string): void;
+    start(): void;
+    stop(): void;
+    getPorts(): void;
+    setPorts(ports: { gateway?: number, api?: number, swarm?: number }, restart: boolean): void;
+    checkVersion(): void;
+}

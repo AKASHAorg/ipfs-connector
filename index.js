3a864d0d5ed14a8a645cf237724e35db930ae015
Marius Darila <marius.darila@gmail.com>
2017-05-04 18:44:05 +0300

    chore(): cleanup

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var IpfsConnector_1 = require("./src/IpfsConnector");
-exports.IpfsConnector = IpfsConnector_1.IpfsConnector;
-var IpfsBin_1 = require("./src/IpfsBin");
-exports.IpfsBin = IpfsBin_1.IpfsBin;
-var constants_1 = require("./src/constants");
-exports.ipfsEvents = constants_1.events;
